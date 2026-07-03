501205f1a953b42c6464a0f0afbbc0eba1a78521
NightNMare <dlarbdnjs1@naver.com>
2026-07-03 18:27:49 +0900

    Fixed : remove wind in generator weather

--- app/pages/generator.vue
+++ app/pages/generator.vue
@@ -74,7 +74,7 @@
                 <option value="비">비</option>
                 <option value="눈">눈</option>
                 <option value="안개">안개</option>
-                <option value="강풍">강풍</option>
+                <!-- <option value="강풍">강풍</option> -->
                 <!-- <option value="결빙">결빙</option> -->
               </select>
             </div>
